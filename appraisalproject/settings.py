0e871d385aef3f1e70733bbd80c14f40457086d7
felkiriinya <felkiriinya@gmail.com>
2020-12-07 23:16:03 +0300

    added and styled the login page

--- appraisalproject/settings.py
+++ appraisalproject/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'bootstrap3',
     'appraisalapp.apps.AppraisalappConfig',
     'cloudinary',
     'django.contrib.admin',
